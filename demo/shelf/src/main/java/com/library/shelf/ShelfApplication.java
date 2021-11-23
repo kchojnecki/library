5a4079ef2d05cdd653724d7e4e292ff042810502
Konrad Chojnecki <konrad.chojnecki@gmail.com>
2021-11-23 22:14:55 +0100

    add shelf app

---
+++ demo/shelf/src/main/java/com/library/shelf/ShelfApplication.java
@@ -0,0 +1,13 @@
+package com.library.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfApplication.class, args);
+    }
+
+}
